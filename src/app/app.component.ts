5db1530ac72b8dd8aa332a9466a10dbb946af63f
DESKTOP-P4FL1OC\Rasika <amilajayamaha96@gmail.com>
2023-06-25 10:24:35 +0530

    add a component folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<div><h3>This is Inline template</h3></div>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
