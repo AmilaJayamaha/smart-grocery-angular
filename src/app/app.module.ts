d6e366bd2094d16c9947cde10e072db27226c20d
DESKTOP-P4FL1OC\Rasika <amilajayamaha96@gmail.com>
2023-06-27 10:18:09 +0530

    Data Binding and Pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
+import { ContentLimitterPipe } from './pipe/content-limitter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    ContentLimitterPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pipe/content-limitter.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'contentLimitter',
+})
+export class ContentLimitterPipe implements PipeTransform {
+  transform(value: string): string {
+    if (value.length > 50) {
+      return value.substring(0, 49) + '...';
+    }
+    return value;
+  }
+}
